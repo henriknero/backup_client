2593f19397e83e0ef55b17fa93109a03f5cf2a16
Henrik Nero <henb14@student.bth.se>
2018-05-02 22:46:39 +0200

    Just updating .gitignore and put some stuff in main

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import Tkinter as tkinter
-import pygit2
-import watchdog
+import tkinter
+from backup_client.filehandler import observer
+#import pdb;pdb.set_trace()
+observer = observer.FileObserver()
+observer.addDir('.')
+observer.start()
+
+def doStuffPls():
+    observer.addFile('.\\fuckme.txt')
+    
 
 top = tkinter.Tk()
+button = tkinter.Button(top,text='Pls press me', command=doStuffPls)
+button.pack()
 top.mainloop()
