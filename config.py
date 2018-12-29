39ccc3bd4de5d58319b824a28669455cfe5d0a4e
Henrik Nero <henriknero@gmail.com>
2018-12-29 22:52:51 +0100

    Rework on the config-file

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
-API='https://nerobp.xyz/gogs/api/v1'
-ROOT='https://nerobp.xyz/gogs/'
-UPDATE_INTERVAL=0 #Update interval set in minutes
+from os.path import expanduser, join
+import configparser
+
+home_config = join(expanduser('~'), '.config/gibc/config')
+
+config = configparser.SafeConfigParser()
+config.read(('default_config','/opt/gibc/default_config', home_config))
+API = config['NET']['API']
+ROOT = config['NET']['ROOT']
+UPDATE_INTERVAL = int(config['NET']['UPD_INTERVAL']) #Update interval set in minutes
+
+ICON = config['FILE']['ICON']
