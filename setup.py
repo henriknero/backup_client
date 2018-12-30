5eee9549ff07a9a06c662cc14cd246963791ae29
Henrik Nero <henriknero@gmail.com>
2018-12-30 17:49:03 +0100

    Setup-file for Linux install

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+from setuptools import setup
+
+setup(
+    name='backup_client',
+    version='0.4',
+    author='Henrik Nero',
+    author_email='henriknero@gmail.com',
+    packages=['backup_client','backup_client.filehandler', 'backup_client.network'],
+    scripts=['gibc'],
+    data_files=[('.', ['default.conf']),
+        ('backup_client', ['backup_client/icon.png'])]
+)
